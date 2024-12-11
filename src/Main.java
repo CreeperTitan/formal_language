105c5ba243b5f5ac8deebdf5ab7bbc771682dba9
stagnater <105478548+CreeperTitan@users.noreply.github.com>
2024-12-11 22:35:26 +0800

    初步大纲

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    //我喜欢你
+    public static void main(String[] args) {
+        //输入处理
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Hello World");
+        //正则表达式--NFA
+
+        //NFA--DFA
+
+        //DFA最小化
+
+        //输出最小化DFA
+
+        //输出正则文法
+    }
+}
